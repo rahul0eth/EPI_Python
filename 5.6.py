000e43441e14796f4d860a6779bc3405342370a8
Rahul Bajaj <rbajaj30@gatech.edu>
2022-07-08 19:04:36 -0500

    brute force

--- 5.6.py
+++ 5.6.py
@@ -1,10 +1,18 @@
 def main():
     A = [310,315,275,295,260,270,290,230,255,250]
 
-    maxprofit_RB(A)
+    maxprofit_RB_bruteforce(A)
 
-def maxprofit_RB(A):
-    print("hello")
+def maxprofit_RB_bruteforce(A):
+
+    max_profit = 0
+
+    for i in range(0,len(A)-1):
+        for j in range(i,len(A)-1):
+            if A[j] - A[i] > max_profit:
+                max_profit = A[j] - A[i]
+
+    print(max_profit)
 
 if __name__ == "__main__":
     main()
